fa56e19679de542ec65d5e6859a7d2f585493506
prokosim <prokosim@gmail.com>
2025-01-31 11:55:57 +0100

    like this - instances can have their properties set up after they're created

--- turret_manager.gd
+++ turret_manager.gd
@@ -3,8 +3,8 @@ extends Node3D
 @export var turret: PackedScene 
 @export var enemy_path: Path3D
 
-func build_turret(turret_position, enemy_path: Path3D) -> void:
-	var enemy_path_ref = enemy_path
+func build_turret(turret_position) -> void:
 	var new_turret = turret.instantiate()
 	add_child(new_turret)
 	new_turret.global_position = turret_position
+	new_turret.enemy_path = enemy_path
