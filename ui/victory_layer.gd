d0191e168ea866ca0d52558c27d0bc922905e0f5
prokosim <prokosim@gmail.com>
2025-02-02 15:19:49 +0100

    Victory with 3 stars

--- ui/victory_layer.gd
+++ ui/victory_layer.gd
@@ -4,14 +4,19 @@ extends CanvasLayer
 @onready var start_2: TextureRect = %Start2
 @onready var start_3: TextureRect = %Start3
 @onready var health_label: Label = %HealthLabel
+@onready var money_label: Label = %MoneyLabel
 
 @onready var base = get_tree().get_first_node_in_group("base")
+@onready var bank = get_tree().get_first_node_in_group("bank")
 
 func victory() -> void:
 	visible = true
 	if base.current_health == base.max_health:
 		start_2.modulate = Color.WHITE
 		health_label.visible = true
+		if bank.gold > 500:
+			start_3.modulate = Color.WHITE_SMOKE
+			money_label.visible = true
 		
 
 func _on_retry_button_pressed() -> void:
